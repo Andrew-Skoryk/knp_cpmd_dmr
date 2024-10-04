f1cab968bd78dbe804a8aac13b86943de750119e
Andrew Skoryk <andrewtimelessone@gmail.com>
2024-10-04 20:20:57 +0300

    feat: favicon, public/images, logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,31 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Header = () => {
   return (
     <header className="bg-white shadow-sm">
       <div className="container flex items-center justify-between px-4 py-4 mx-auto ">
-        <Link href="/" className="max-w-2xl text-2xl font-bold text-primary">
-          КОМУНАЛЬНЕ НЕКОМЕРЦІЙНЕ ПІДПРИЄМСТВО «ЦЕНТР ПЕРВИННОЇ МЕДИЧНОЇ
-          ДОПОМОГИ» ДОЛИНСЬКОЇ МІСЬКОЇ РАДИ
-        </Link>
+        <div className="flex items-center space-x-4">
+          <Link href="/">
+            <Image
+              src="/images/knp_logo.jpg"
+              alt="Лого центру"
+              width={110}
+              height={110}
+              className="object-contain"
+            />
+          </Link>
+
+          <span className="text-xl font-bold leading-tight text-primary">
+            КОМУНАЛЬНЕ НЕКОМЕРЦІЙНЕ ПІДПРИЄМСТВО
+            <br />
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-green-700">
+              «ЦЕНТР ПЕРВИННОЇ МЕДИЧНОЇ ДОПОМОГИ»
+            </span>
+            <br />
+            ДОЛИНСЬКОЇ МІСЬКОЇ РАДИ
+          </span>
+        </div>
 
         <nav>
           <ul className="flex space-x-4">
